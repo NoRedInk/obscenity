44c17dd73b67b1586a0e6e5cd6f2a480dbda92b4
Michael Glass <me@mike.is>
2014-12-12 21:56:44 -0800

    performance improvements

--- lib/obscenity/base.rb
+++ lib/obscenity/base.rb
@@ -3,11 +3,12 @@ module Obscenity
     class << self
 
       def blacklist
-        @blacklist ||= set_list_content(Obscenity.config.blacklist)
+        @blacklist ||= set_list_content(Obscenity.config.blacklist) - whitelist
       end
 
       def blacklist=(value)
         @blacklist = value == :default ? set_list_content(Obscenity::Config.new.blacklist) : value
+        @blacklist_pattern = nil
       end
 
       def whitelist
@@ -16,12 +17,22 @@ module Obscenity
 
       def whitelist=(value)
         @whitelist = value == :default ? set_list_content(Obscenity::Config.new.whitelist) : value
+        @whitelist_pattern = nil
+      end
+
+      def blacklist_pattern
+        @blacklist_pattern ||= /\b(#{blacklist.map {|a| "(?:#{a})"}.join('|')})\b/i
+      end
+
+      def whitelist_pattern
+        @whitelist_pattern ||= /\b(#{whitelist.map {|a| "(?:#{a})"}.join('|')})\b/
       end
 
       def profane?(text)
-        return(false) unless text.to_s.size >= 3
-        blacklist.each do |foul|
-          return(true) if text =~ /\b#{foul}\b/i && !whitelist.include?(foul)
+        return false unless text.to_s.size >= 3
+        text.split.each do |piece|
+          next if piece =~ whitelist_pattern
+          return true if piece =~ blacklist_pattern
         end
         false
       end
